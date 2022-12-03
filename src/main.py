77cab3eeba6dc46e8c24c69e22fe5c27ebb0dd99
Runming Li <runmingl@andrew.cmu.edu>
2022-12-03 16:52:09 -0500

    add visualizer

--- src/main.py
+++ src/main.py
@@ -1,21 +1,34 @@
+import fire
+
 from ltl_regex import *
 from ltl2aut import ltl_to_aut, aut_to_graph
 from aut2regex import aut_to_regex
+from vis import make_graph
 
-def test(formula: str):
-    print(aut_to_regex(aut_to_graph(ltl_to_aut(formula))))
+class Ltl2Regex(object):
+    def ltl2regex(self, formula: str):
+        return aut_to_regex(aut_to_graph(ltl_to_aut(formula)))
 
-test('G(a & b)')
-test('G(a & b) & F(c & d)')
-test('GFa -> GFb')
-test('G(p xor Xp)')
-test('0')
-test('0 R p1')
-test('F(XG(F!p1 M Fp1) W (p1 R p0))')
-test('F(p0 R !p2)')
-test('G(p0 | Fp1) W (FGp1 R !p1)')
-test('X!(Xp0 U !p2)')
-test('Fp2 | !(X(p2 xor Gp2) R (p1 W !Fp2))')
-test('XXG((Xp1 R (p0 W p2)) -> (p2 R GXp0))')
-test('X(0)')
-test('XXF(p0 R p1)')
+    def ltl2timeline(self, formula: str, filename: str = "ltl", output_format: str = "pdf"):
+        g = make_graph(aut_to_regex(aut_to_graph(ltl_to_aut(formula))), filename, output_format)
+        g.view()
+
+if __name__ == '__main__':
+    fire.Fire(Ltl2Regex)
+
+
+
+# print(Ltl2Regex.ltl2regex('G(a & b)'))
+# print(Ltl2Regex.ltl2regex('G(a & b) & F(c & d)'))
+# print(Ltl2Regex.ltl2regex('GFa -> GFb'))
+# print(Ltl2Regex.ltl2regex('G(p xor Xp)'))
+# print(Ltl2Regex.ltl2regex('0'))
+# print(Ltl2Regex.ltl2regex('0 R p1'))
+# print(Ltl2Regex.ltl2regex('F(XG(F!p1 M Fp1) W (p1 R p0))'))
+# print(Ltl2Regex.ltl2regex('F(p0 R !p2)'))
+# print(Ltl2Regex.ltl2regex('G(p0 | Fp1) W (FGp1 R !p1)'))
+# print(Ltl2Regex.ltl2regex('X!(Xp0 U !p2)'))
+# print(Ltl2Regex.ltl2regex('Fp2 | !(X(p2 xor Gp2) R (p1 W !Fp2))'))
+# print(Ltl2Regex.ltl2regex('XXG((Xp1 R (p0 W p2)) -> (p2 R GXp0))'))
+# print(Ltl2Regex.ltl2regex('X(0)'))
+# print(Ltl2Regex.ltl2regex('XXF(p0 R p1)'))
---
+++ src/vis.py
@@ -0,0 +1,70 @@
+from graphviz import Digraph
+from ltl_regex import *
+from typing import List, Tuple
+
+node_counter = 0
+cluster_counter = 0
+
+def make_graph(regex: OmegaRegex, filename: str, format: str):
+    g = Digraph('G', filename=f'{filename}.gv', format=format)
+    g.attr(rankdir='LR')
+
+    def make_omega_regex_graph(r: OmegaRegex, parents: List[str]) -> Digraph:
+        global cluster_counter
+        match r:
+            case Repeat(re):
+                with g.subgraph(name=f'cluster_{cluster_counter}') as c:
+                    cluster_counter += 1
+                    _, heads = make_regex_graph(c, re, parents, connect_parent=False)
+                    c.attr(style='filled', color='lightgrey')
+                    c.node_attr.update(style='filled', color='white')
+                for head in heads:
+                    for parent in parents:
+                        g.edge(parent, head)
+            case ConcatOmega(left, right):
+                node1_tail, _ = make_regex_graph(g, left, parents, connect_parent=True)
+                make_omega_regex_graph(right, node1_tail)
+            case UnionOmega(left, right):
+                make_omega_regex_graph(left, parents)
+                make_omega_regex_graph(right, parents)
+
+    def make_regex_graph(g: Digraph, r: Regex, parents: List[str], connect_parent: bool = True) -> Tuple[List[str], List[str]]:
+        global node_counter
+        match r:
+            case Epsilon():
+                node_name = str(node_counter)
+                g.node(node_name, 'ε')
+                node_counter += 1
+                if connect_parent:
+                    for parent in parents:
+                        g.edge(parent, node_name)
+                return [node_name], [node_name]
+            case Symbol(s):
+                node_name = str(node_counter)
+                g.node(node_name, s)
+                node_counter += 1
+                if connect_parent:
+                    for parent in parents:
+                        g.edge(parent, node_name)
+                return [node_name], [node_name]
+            case Concat(r1, r2):
+                node1_tail, node1_head = make_regex_graph(g, r1, parents, connect_parent)
+                node2_tail, _          = make_regex_graph(g, r2, node1_tail, connect_parent=True)
+                return node2_tail, node1_head
+            case Union(r1, r2):
+                node1_tail, node1_head = make_regex_graph(g, r1, parents, connect_parent)
+                node2_tail, node2_head = make_regex_graph(g, r2, parents, connect_parent)
+                return list(set(node1_tail + node2_tail)), list(set(node1_head + node2_head))
+            case Star(r):
+                node1_tail, node1_head = make_regex_graph(g, r, parents, connect_parent)
+                node_name = str(node_counter)
+                g.node(node_name, '...')
+                node_counter += 1
+                for tail in node1_tail:
+                    g.edge(tail, node_name)
+                node2_tail, node2_head = make_regex_graph(g, r, [node_name], connect_parent=True)
+                return node2_tail, node2_head
+
+    g.node('start', 'start')
+    make_omega_regex_graph(regex, ['start'])
+    return g
